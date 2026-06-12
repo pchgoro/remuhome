ae3c056f57146f4b12e17b275bff44f83eb6d31a
pchgoro <goro30422@gmail.com>
2026-06-12 15:51:12 +0900

    remove notification test

--- api/cron-check.js
+++ api/cron-check.js
@@ -106,12 +106,6 @@ export default async function handler(req, res) {
 
   const notifications = [];
 
-notifications.push({
- title:'通知テスト',
- body:'ここが届けば完成',
- url:'/'
-});
-
   // Check YouTube Live
   if (ytData && !ytData.error) {
     if (ytLiveId && ytLiveId !== lastCheckedState.youtube_live) {
