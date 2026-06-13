6abaa7e86add6fef744612dd40bf2965beab0313
pchgoro <goro30422@gmail.com>
2026-06-13 18:00:12 +0900

    feat: bump service worker cache version to v2 and force promotion

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'remuhome-cache-v1';
+const CACHE_NAME = 'remuhome-cache-v2';
 const ASSETS_TO_CACHE = [
   '/',
   '/index.html',
@@ -12,7 +12,7 @@ self.addEventListener('install', event => {
   event.waitUntil(
     caches.open(CACHE_NAME).then(cache => {
       return cache.addAll(ASSETS_TO_CACHE);
-    })
+    }).then(() => self.skipWaiting())
   );
 });
 
@@ -26,7 +26,7 @@ self.addEventListener('activate', event => {
           }
         })
       );
-    })
+    }).then(() => self.clients.claim())
   );
 });
 
